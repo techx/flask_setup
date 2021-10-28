ede5c8816c6212b3573313ee4ef59b8bc8a0d0b6
AnirudhRahul <anirudhrahul@anjurahul.com>
2021-10-28 18:04:32 -0400

    Port 80 default

--- run.py
+++ run.py
@@ -5,5 +5,5 @@ import os
 app = create_app()
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5100))
+    port = int(os.environ.get("PORT", 80))
     app.run(host="0.0.0.0", port=port, debug=False)
