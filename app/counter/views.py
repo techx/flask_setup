dae36d39507e8722a0a1b221756a779ab0bd8bfc
AnirudhRahul <anirudhrahul@anjurahul.com>
2021-10-28 18:11:08 -0400

    Updated game

--- app/counter/views.py
+++ app/counter/views.py
@@ -4,11 +4,18 @@ counter_bp = Blueprint("counter", __name__, url_prefix='/count')
 
 counts = {}
 
-@counter_bp.route("/", methods=['GET'])
-def hello():
+from math import sqrt
+from itertools import count, islice
+def is_prime(n):
+    return n > 1 and all(n % i for i in islice(count(2), int(sqrt(n) - 1)))
+
+@counter_bp.route("/the/super/secret/endpoint/only/ani/knows/about/lol/this/is/so/scuffed", methods=['GET'])
+def secret():
     res = "These are the current counts!<br>"
-    for person in counts:
-        res += person + ": " + str(counts[person]) + "<br>"
+    lis = sorted([ (count if is_prime(count) else -1, name) for name,count in counts.items()], reverse=True)
+
+    for count, person in lis:
+        res += person + ": " + str(count) + "<br>"
     return res, 200
 
 @counter_bp.route("/add/<user>", methods=["GET"])
@@ -19,9 +26,9 @@ def add_user(user):
         counts[user]=1    
     return "Done!", 200
 
-@counter_bp.route("/get/<user>", methods=["GET"])
-def get_user(user):
-    if user in counts:
-        return str(counts[user]), 200
-    else:
-        return "Not found :(", 404
+# @counter_bp.route("/get/<user>", methods=["GET"])
+# def get_user(user):
+#     if user in counts:
+#         return str(counts[user]), 200
+#     else:
+#         return "Not found :(", 404
