449452e199645f6934811355fb1c12f7903dc994
AnirudhRahul <anirudhrahul@anjurahul.com>
2021-10-28 13:25:03 -0400

    Counting Server

--- app/__init__.py
+++ app/__init__.py
@@ -1,40 +1,20 @@
 import os
 from flask import Flask, session, request
-from flask_sqlalchemy import SQLAlchemy
 
 app = None
 db = None
 
 
-def load_from_env(app, *args):
-    for a in args:
-        app.config[a] = os.environ[a]
-
 def create_app():
     global app, db, session
     app = Flask(__name__)
 
-    # load config vars from file or environment
-    if os.path.exists("config.py"):
-        app.config.from_pyfile("../config.py")
-        print("Loading secret configs from file")
-    else:
-        load_from_env(app, 'FLASK_SECRET_KEY',
-                                    'DEBUG',
-                                    'SQLALCHEMY_DATABASE_URI') 
-        print("Loading secret configs from env")
-
-    # Database Setup (uncomment after database set in SQLALCHEMY_DATABASE_URI)
-    #load database
-    db = SQLAlchemy(app)
-    from app.models import Item
-
-    db.create_all()
+    @app.errorhandler(404)
+    def page_not_found(err):
+            return 'RIP 404', 404
 
     # register module blueprints
-    from app.module1.views import module1_bp
-    from app.module2.views import module2_bp
-    app.register_blueprint(module1_bp)
-    app.register_blueprint(module2_bp)
+    from app.counter.views import counter_bp
+    app.register_blueprint(counter_bp)
 
     return app
---
+++ app/counter/views.py
@@ -0,0 +1,27 @@
+from flask import Blueprint, request
+
+counter_bp = Blueprint("counter", __name__, url_prefix='/count2')
+
+counts = {}
+
+@counter_bp.route("/", methods=['GET'])
+def hello():
+    res = "These are the current counts!<br>"
+    for person in counts:
+        res += person + ": " + str(counts[person]) + "<br>"
+    return res, 200
+
+@counter_bp.route("/add/<user>", methods=["GET"])
+def add_user(user):
+    if user in counts:
+        counts[user]+=1
+    else:
+        counts[user]=1    
+    return "Done!", 200
+
+@counter_bp.route("/get/<user>", methods=["GET"])
+def get_user(user):
+    if user in counts:
+        return str(counts[user]), 200
+    else:
+        return "Not found :(", 404
